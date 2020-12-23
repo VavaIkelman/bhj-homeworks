5980f3d0905a1745e8034f93f37c924f7002de7b
VavaIkelman <mr.ilkevich@gmai.com>
2020-12-23 22:57:43 +0300

    Added HomeWork BHJ-16 1.1

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,22 +1,9 @@
-const cuntTime = document.getElementById("timer");
-function showTimer(cunt){
-        cunt = Math.round(startTimer);
-        let hours = Math.floor(secs / (60 * 60));
-        let divisor_for_minutes = secs % (60 * 60);
-        let minutes = Math.floor(divisor_for_minutes / 60);
-    
-        let divisor_for_seconds = divisor_for_minutes % 60;
-        let seconds = Math.ceil(divisor_for_seconds);
-        return `${hours}: ${minutes}: ${seconds}`;    
-    }
-
-const showWin = function(){
-    cuntTime.textContent -= 1;
-     
-    if (cuntTime.textContent == 0){
+let x = setInterval(function(){ 
+    const countTime = document.getElementById("timer");
+    countTime.textContent -= 1;  
+    if (countTime.textContent == 0){
         alert(`Вы победили в конкурсе!`);
-        clearInterval(timerId);
-        location.reload(true)
-    }
-}
-let timerId = setInterval(showWin, 1000);
+        clearInterval(x);
+        location.reload(true);
+    };
+}, 1000);
