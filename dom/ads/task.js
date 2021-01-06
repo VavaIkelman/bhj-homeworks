25854a88d79902d39053ed327dc31734456c4f0f
VavaIkelman <mr.ilkevich@gmail.com>
2021-01-06 16:05:56 +0300

    Added homework DOM 2.2

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,2 +1,25 @@
-let b = [2, 1, 3];
-console.log(b)
+class PolySwitch {
+	constructor(root, delay) {
+  	this.root = root;
+    this.delay = delay;
+    this.isRunning = true;
+    this.timeout = setTimeout(this.loop.bind(this), this.delay);
+  }
+  
+  loop() {
+  	this.setActiveElement();
+    
+    if(this.isRunning)
+    	this.timeout = setTimeout(this.loop.bind(this), this.delay);
+  }
+  
+	setActiveElement(element) {
+  	const active = this.root.querySelector(".rotator__case_active");
+  	const next = element || this.root.querySelector(".rotator__case_active + .rotator__case") || this.root.querySelector(".rotator__case");
+  
+  	active.classList.remove("rotator__case_active");
+ 	next.classList.add("rotator__case_active");
+  }
+}
+
+new PolySwitch(document.querySelector(".rotator"), 1000);
