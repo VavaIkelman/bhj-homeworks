b0a2a12eed197b25a499fa3e9fa52052ba877938
VavaIkelman <mr.ilkevich@gmail.com>
2021-01-13 18:19:39 +0300

    Added hw 1.2.2

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,8 +1,11 @@
 const menuLink = document.querySelectorAll('.menu__link');
-// const menuLink = document.getElementsByTagName('a');
+const menuLinkActive = document.getElementsByClassName('menu menu_sub menu_active');
 [].forEach.call(menuLink, function(el){
 for (let i of menuLink){
 i.onclick = function(){
+    for (let n  of menuLinkActive){
+        menuLinkActive[n].classList.remove('menu_active');
+    };
     if (i.nextElementSibling.className === 'menu menu_sub'){
         i.closest('li').querySelector('.menu .menu_sub').classList.add('menu_active');
     } else {
