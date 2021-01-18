24a17a21a63f46401d9ba829ef6ad1ef13438c78
VavaIkelman <mr.ilkevich@gmail.com>
2021-01-18 15:16:39 +0300

    Added hw 1.2.3 работа над ошибками

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,5 +1,5 @@
 const slides = document.querySelectorAll('.slider__item');
-const dots = document.querySelectorAll('.slider__dot');
+const dots = [...document.querySelectorAll('.slider__dot')];
 const next = document.querySelector('.slider__arrow_next');
 const prev = document.querySelector('.slider__arrow_prev');
 let currentSlide = 0;
@@ -22,13 +22,14 @@ prev.onclick = () => {
 }
 
 dots[currentSlide].classList.add('slider__dot_active');
-[...dots].forEach((item, i) => item.onclick = () => {
+dots.forEach((item, i) => item.onclick = () => {
     showSlide(i);
+    currentSlide = i;
 });
 
 function showSlide(i){
     [...slides].forEach((item) => item.classList.remove('slider__item_active'));
-    [...dots].forEach((item) => item.classList.remove('slider__dot_active'));
+    dots.forEach((item) => item.classList.remove('slider__dot_active'));
     slides[i].classList.add('slider__item_active');
     dots[i].classList.add('slider__dot_active');
 }
