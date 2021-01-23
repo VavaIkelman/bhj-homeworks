10457d0629ec07e5cd016bdf9e4a19da61e3f677
VavaIkelman <mr.ilkevich@gmail.com>
2021-01-23 18:58:28 +0300

    Added hw 2.1.2

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,7 +1,7 @@
 class PolySwitch {
 	constructor(root, delay) {
   	this.root = root;
-    this.delay = delay;
+	this.delay = document.querySelector('.rotator__case_active').getAttribute('data-speed');
     this.isRunning = true;
     this.timeout = setTimeout(this.loop.bind(this), this.delay);
   }
@@ -9,17 +9,18 @@ class PolySwitch {
   loop() {
   	this.setActiveElement();
     
-    if(this.isRunning)
+    if(this.isRunning){
     	this.timeout = setTimeout(this.loop.bind(this), this.delay);
   }
+}
   
 	setActiveElement(element) {
   	const active = this.root.querySelector(".rotator__case_active");
   	const next = element || this.root.querySelector(".rotator__case_active + .rotator__case") || this.root.querySelector(".rotator__case");
-  
+	active.style.color = active.getAttribute('data-color');
   	active.classList.remove("rotator__case_active");
  	next.classList.add("rotator__case_active");
   }
 }
 
-new PolySwitch(document.querySelector(".rotator"), 1000);
+new PolySwitch(document.querySelector(".rotator"), this.delay);
