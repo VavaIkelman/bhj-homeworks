2790d67d7486900d437fa75c1c5c3b96a2e12c4b
VavaIkelman <mr.ilkevich@gmail.com>
2021-02-04 22:35:03 +0300

    Исправлены замечание к заданию 2.3.1

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -4,11 +4,11 @@ tooltip.className = 'tooltip';
 
 [...links].forEach((item) => {
     item.addEventListener('click', (e) => {
-        e.preventDefault();
-        tooltip.classList.add('tooltip_active');
-        if (item.nextElementSibling === 'tooltip tooltip_ctive'){
+        e.preventDefault();     
+        if (item.nextElementSibling.className === 'tooltip tooltip_active'){
             item.nextElementSibling.classList.remove('tooltip_active');
         } else {
+            tooltip.classList.add('tooltip_active');
             tooltip.textContent = item.title;
             tooltip.style.left = item.offsetLeft + document.body.scrollLeft + "px";
             tooltip.style.top = item.offsetTop + document.body.scrollTop + 20 + 'px';
