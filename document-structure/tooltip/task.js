3ee9461a60128ca346e4016e04d78f8771f6102e
VavaIkelman <mr.ilkevich@gmail.com>
2021-02-04 17:01:28 +0300

    Исправлены замечание к заданию 2.3.1

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -4,17 +4,15 @@ tooltip.className = 'tooltip';
 tooltip.style.display = 'block';
 
 [...links].forEach((item) => {
-    let top = item.getBoundingClientRect().top;
-     if (top > 0 && top < window.innerHeight){
-        item.insertAdjacentElement('afterend', tooltip);
-        item.addEventListener('click', (e) => {
-            e.preventDefault();
-            if (item.nextElementSibling === 'tooltip'){
-                item.nextElementSibling.remove();
-            } else {
-                tooltip.textContent = item.title;
-                item.after(tooltip);
-            }
-        });
-    }
+    item.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (item.nextElementSibling === 'tooltip'){
+            item.nextElementSibling.remove();
+        } else {
+            tooltip.textContent = item.title;
+            tooltip.style.left = item.offsetLeft + document.body.scrollLeft + "px";
+            tooltip.style.top = item.offsetTop + document.body.scrollTop + 20 + 'px';
+            item.after(tooltip);
+        }
+    });
 });
