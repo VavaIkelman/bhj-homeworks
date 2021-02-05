9b451675fadf5ae750ce6edccdb264fca1f605ea
VavaIkelman <mr.ilkevich@gmail.com>
2021-02-05 19:38:55 +0300

    Исправлены замечание к заданию 2.3.1

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,19 +1,20 @@
-const links = document.querySelectorAll('.has-tooltip');
+const links = document.querySelectorAll('.has-tooltip'),
+      active = document.getElementsByClassName('tooltip tooltip_active');
 const tooltip = document.createElement('div');
 tooltip.className = 'tooltip';
 
 [...links].forEach((item) => {
     item.addEventListener('click', (e) => {
-        e.preventDefault();     
-        if (item.nextElementSibling.className === 'tooltip tooltip_active'){
-            item.nextElementSibling.classList.remove('tooltip_active');
-        } else {
-            tooltip.classList.add('tooltip_active');
-            tooltip.textContent = item.title;
-            tooltip.style.left = item.offsetLeft + document.body.scrollLeft + "px";
-            tooltip.style.top = item.offsetTop + document.body.scrollTop + 20 + 'px';
-            item.after(tooltip);
-        }
+        e.preventDefault();  
+        if (tooltip.innerText === item.title){
+            tooltip.classList.remove('tooltip_active');
+            return
+        } 
+        tooltip.classList.add('tooltip_active');
+        tooltip.textContent = item.title;
+        tooltip.style.left = item.offsetLeft + document.body.scrollLeft + "px";
+        tooltip.style.top = item.offsetTop + document.body.scrollTop + 20 + 'px';
+        item.after(tooltip);
     });
 });
 window.addEventListener('scroll', (e) => {
