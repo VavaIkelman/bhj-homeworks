4e6901f91c80c5ea7cb15372cc495d4c53ece29c
VavaIkelman <mr.ilkevich@gmail.com>
2021-02-08 23:11:08 +0300

    Добавлено ДЗ 3.2

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -8,32 +8,13 @@ const btnClose = document.querySelector('.modal__close');
         modalMain.className('modal');
      };
 
-// const getCookie = (name) => {
-//     const value = "; " + document.cookie;
-//     let parts = value.split("; " + name + "="); if (parts.length === 2) {
-//     return parts.pop().split(";").shift(); 
-//     }
-// }
-
 function getCookie(name) {
     let matches = document.cookie.match(new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"));
     return matches ? decodeURIComponent(matches[1]) : undefined;
   }
 
 btnClose.addEventListener('click', (e) => {
-e.preventDefault();
-    // setCookie('popup', 'closed');
+    e.preventDefault();
     document.cookie = 'popup=closed';
     modalMain.classList.remove('modal_active')
 });
-   
-// function setCookie(name, value){
-//     let cookieString = name + "=" + escape(value);
-//     document.cookie = cookieString;
-// }
-
-function deleteCookie(name){ //удаление куки для самопроверки
-    let cookieDate = new Date();
-    cookieDate.setTime(cookieDate.getTime() - 1);
-    document.cookie = name += "=; expires=" + cookieDate.toGMTString();
-}
